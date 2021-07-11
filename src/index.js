83004b1d497d57b1917628c2df5b36fc84f2903d
Nader Chargui <charguinader19901403@gmail.com>
2021-07-11 16:46:02 +0200

    update for comment  for ()

--- src/index.js
+++ src/index.js
@@ -21,3 +21,20 @@ reactDOM.render(ele, document.getElementById('root'))
 // npm i  babel-preset-react   babel-preset-env
 
 //  npx babel  src/index.js  --out-file=public/js/main.js  --presets=env,react
+
+//  bech  tektab akther min stat  lazem taa3mil la9wass ()  w labed min parent 
+/**
+ * 
+ * (
+ *  <div>
+ *    <h1>
+ *     
+ *   hello  jsx 
+ * </h1>
+ * <p>
+ *  lorem   lorem  
+ * </p>
+ * </div>
+ * 
+ * )
+ */
