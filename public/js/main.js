21970a9fd2acc0bbdf6b17d0043dde65773b3cab
Nader Chargui <charguinader19901403@gmail.com>
2021-07-11 17:13:01 +0200

    law hatha mawjoud otb3ly ely baa3dou { var && ' write something here'

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,36 @@
-'use strict';
+"use strict";
 
+var title = "react js";
+var fullName = "Nader Chargui";
+var fun = function fun() {
+    return "i'm nader learn react js ";
+};
 var ele = React.createElement(
-  'h1',
-  null,
-  '  welcome react js '
+    "div",
+    null,
+    React.createElement(
+        "h2",
+        null,
+        "heloo  ",
+        title
+    ),
+    React.createElement(
+        "h1",
+        null,
+        "  welcome react js "
+    ),
+    React.createElement(
+        "p",
+        null,
+        "lorem loremloremlorem lorem lorem ",
+        3 + 3
+    ),
+    fun(),
+    React.createElement("br", null),
+    fullName && "oky  is  true Name"
 );
 
-reactDOM.render(ele, document.getElementById('root'));
+ReactDOM.render(ele, document.getElementById('root'));
 
 //jsx to  js  by babel  for browser understand this code jsx
 
@@ -14,3 +38,20 @@ reactDOM.render(ele, document.getElementById('root'));
 // npm i  babel-preset-react   babel-preset-env
 
 //  npx babel  src/index.js  --out-file=public/js/main.js  --presets=env,react
+//  npx babel  src/index.js  --out-file=public/js/main.js  --presets=env,react  --watch
+//  bech  tektab akther min stat  lazem taa3mil la9wass ()  w labed min parent 
+/**
+ * 
+ * (
+ *  <div>
+ *    <h1>
+ *     
+ *   hello  jsx 
+ * </h1>
+ * <p>
+ *  lorem   lorem  
+ * </p>
+ * </div>
+ * 
+ * )
+ */
--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
-var ele  = <h1>  welcome react js </h1>
+  let  title = "react js"
+  let fullName  = "Nader Chargui"
+   const fun = ()  => {
+       return `i'm nader learn react js `
+   }
+var ele  = (
 
-reactDOM.render(ele, document.getElementById('root'))
+     <div>
+     <h2>
+         heloo  {title}
+     </h2>
+<h1>  welcome react js </h1>
+   <p>
+
+       lorem loremloremlorem lorem lorem {3 +3 }
+   </p>
+   {fun()}
+   <br/>
+   {/* law  essm  mawjoud  ektebly mbaaed  essm elhwa  mout88air mou3aref */}
+   {fullName &&  "oky  is  true Name"}
+     </div>
+)
+
+ReactDOM.render(ele, document.getElementById('root'))
 
 
 
@@ -21,7 +42,7 @@ reactDOM.render(ele, document.getElementById('root'))
 // npm i  babel-preset-react   babel-preset-env
 
 //  npx babel  src/index.js  --out-file=public/js/main.js  --presets=env,react
-
+//  npx babel  src/index.js  --out-file=public/js/main.js  --presets=env,react  --watch
 //  bech  tektab akther min stat  lazem taa3mil la9wass ()  w labed min parent 
 /**
  * 
